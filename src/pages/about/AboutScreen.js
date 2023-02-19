6dc28855af50a2225a27bbe9caeccbc2b4a1230c
nadeemarcs <61172555+nadeemarcs@users.noreply.github.com>
2023-02-19 17:48:19 +0530

    Add files via upload

---
+++ src/pages/about/AboutScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PageTitle from "../../components/Common/PageTitle";
+
+function AboutScreen() {
+  return (
+    <div>
+      <div className="p-4">
+        <div className="bg-white rounded-xl p-3 font-title">
+          </div>
+          </div>
+    </div>
+  );
+}
+export default AboutScreen;
